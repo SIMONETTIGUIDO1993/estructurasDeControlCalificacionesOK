4738762d80cce3b8b31715c702ba0ac5debc1f61
SIMONETTIGUIDO1993 <104177337+SIMONETTIGUIDO1993@users.noreply.github.com>
2022-05-13 22:26:04 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# estructurasDeControlCalificacionesOK
+# estructurasDeControlCalificaciones
 Created with CodeSandbox
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+let nombreAlumno: string | null;
+let notaPractica, notaProblemas, notaTeoria, notaFinal: number;
+
+nombreAlumno = prompt("Ingrese el nombre del alumno");
+
+while (nombreAlumno !== "") {
+  notaPractica = Number(prompt("Ingrese nota de Practica"));
+  notaProblemas = Number(prompt("Ingrese nota de Problemas"));
+  notaTeoria = Number(prompt("Ingrese nota de Teoria"));
+
+  if (
+    notaPractica > 10 ||
+    notaPractica < 0 ||
+    notaTeoria > 10 ||
+    notaTeoria < 0 ||
+    notaProblemas > 10 ||
+    notaProblemas < 0
+  ) {
+    console.log(
+      "ERROR: Algunas de las notas ingresadas esta fuera del rango de 0 a 10"
+    );
+  } else {
+    let notaFinal: number =
+      notaPractica * 0.1 + notaProblemas * 0.5 + notaTeoria * 0.4;
+    console.log(
+      "NOTA PROMEDIO FINAL DE " + nombreAlumno + ":" + "$" + notaFinal
+    );
+  }
+  nombreAlumno = prompt("Ingrese el nombre del alumno");
+}
